d4e4171d09172de9a66110112b533b5c97593bf5
Deny Arsyad <deny.arsyad17@gmail.com>
2026-04-10 21:11:52 +0700

    merubah beberapa bagian saat change machine

--- application/views/maintenanceAct/actChanged.php
+++ application/views/maintenanceAct/actChanged.php
@@ -92,6 +92,67 @@
 
 </script>
 
+<!-- Ini untuk PDA -->
+ <script>
+	$(document).ready(function () {
+
+		$('#machine_idScan_input').on('keydown', function (e) {
+
+			if (e.key === 'Enter') {
+				e.preventDefault();
+
+				let machineId = $(this).val().trim().toUpperCase();
+				if (!machineId) return;
+				// console.log(machineId);
+				// console.log("masuk ye");
+				$('#machine_idScan').val(machineId);
+
+				$('#machine_idScan_display').val('*'.repeat(machineId.length));
+
+				$(this).val('');
+
+				processScan(machineId);
+			}
+		});
+
+		function focusScanner() {
+			$('#machine_idScan_input').focus();
+		}
+
+		$('#btnFocusScan').on('click', function () {
+			focusScanner();
+		});
+
+		window.focusScanner = focusScanner;
+
+	});
+
+	function processScan(machineId) {
+		let beep = document.getElementById('beepSound');
+		beep.currentTime = 0;
+		beep.play();
+
+		$.ajax({
+			url: "<?= base_url('maintenance_act/getMachineById'); ?>",
+			type: "POST",
+			data: { machine_idScan: machineId },
+			dataType: "json",
+			success: function (res) {
+				if (res.success) {
+					$('#machine_name_change').val(res.data.machine_name);
+				} else {
+					$('#machine_name_change').val('');
+					alert('Machine ID tidak ditemukan!');
+				}
+			},
+			error: function () {
+				alert('Terjadi kesalahan saat mengambil data.');
+			}
+		});
+	}
+</script>
+<!-- PDA -->
+
 <?php if ($this->session->flashdata('error')): ?>
 <script>
     alert("<?= $this->session->flashdata('error'); ?>");
@@ -145,37 +206,62 @@
 					</div>
 				</div>	
 
-				<div class="form-group">
+				<!-- <div class="form-group">
 					<label class="mb-1 font-weight-bold">Change to Machine <span class="text-danger small">*Required</span></label>
 					<textarea name="change_mc" placeholder="" class="form-control <?= (form_error('change_mc') ? "is-invalid" : "") ?>" rows="2"><?= set_value('change_mc'); ?></textarea>
 					<div class="invalid-feedback">
 						<?= form_error('change_mc'); ?>
 					</div>
-				</div>
+				</div> -->
 
-				<div class="form-group">
-					<label class="mb-1 font-weight-bold">Root Cause <span class="text-danger small">*Required</span></label>
-					<textarea name="root_cause" placeholder="" class="form-control <?= (form_error('root_cause') ? "is-invalid" : "") ?>" rows="2"><?= set_value('root_cause'); ?></textarea>
-					<div class="invalid-feedback">
-						<?= form_error('root_cause'); ?>
+				<div class="row mb-3">
+					<div class="col-md-2">
+						<div class="form-group">
+							<label class="mb-1 d-block">&nbsp;</label> 
+							<button type="button" id="btnFocusScan" class="btn btn-primary btn-block">
+								Focus Scan
+							</button>
+						</div>
+						<div class="row">
+							<div class="col-sm-12">
+								<div id="reader" style="width:300px; display:none;"></div>
+							</div>
+						</div>
+						<audio id="beepSound" preload="auto">
+							<source src="<?= base_url('../../../assets/sound/beepSound.mp3') ?>" type="audio/mpeg">
+						</audio>
+					</div>
+					<div class="col-md-4">
+						<div class="form-group">
+							<label class="mb-1 font-weight-bold">Machine ID</label>
+
+							<input type="text"
+								id="machine_idScan_input"
+								class="form-control"
+								autofocus
+								style="opacity:100; position:absolute; pointer-events:none;">
+
+							<input type="text"
+								id="machine_idScan_display"
+								class="form-control"
+								placeholder="Scan Machine ID"
+								readonly>
+
+							<input type="hidden"
+								id="machine_idScan"
+								name="machine_idScan">
+						</div>
+					</div>
+
+					<div class="col-md-6">
+						<div class="form-group">
+							<label class="mb-1 font-weight-bold">Machine Name</label>
+							<input type="text" id="machine_name_change" name="machine_name_change" class="form-control" ... readonly>
+						</div>
 					</div>
 				</div>
+				
 
-				<div class="form-group">
-					<label class="mb-1 font-weight-bold">Temporary Action <span class="text-danger small">*Required</span></label>
-					<textarea name="temp_act" placeholder="" class="form-control <?= (form_error('temp_act') ? "is-invalid" : "") ?>" rows="2"><?= set_value('temp_act'); ?></textarea>
-					<div class="invalid-feedback">
-						<?= form_error('temp_act'); ?>
-					</div>
-				</div>
-
-				<div class="form-group">
-					<label class="mb-1 font-weight-bold">Preventive Action <span class="text-danger small">*Required</span></label>
-					<textarea name="prev_act" placeholder="" class="form-control <?= (form_error('prev_act') ? "is-invalid" : "") ?>" rows="2"><?= set_value('prev_act'); ?></textarea>
-					<div class="invalid-feedback">
-						<?= form_error('prev_act'); ?>
-					</div>
-				</div>
 				</br>
 				<button type="submit" class="btn btn-success btn-lg mr-2"><i class="fas fa-check"></i> Done With Changed Machine</button>
 
